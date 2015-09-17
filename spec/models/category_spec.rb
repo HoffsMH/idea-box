fca7e7afc4ff062d41c3fbbab7217889f62cb8b3
HoffsMH <hecker.matthew@gmail.com>
2015-09-17 15:18:52 -0600

    made first validation test for categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) do
+    Category.new(name: "Math")
+  end
+
+  it "is valid" do
+    expect(category).to be_valid
+  end
+
+  it "is invalid without a name" do
+    category.name = nil
+    expect(category).not_to be_valid
+  end
+
 end
