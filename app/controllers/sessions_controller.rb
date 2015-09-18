eea11aa4f6a53a8fae20986374a391f978bc7282
HoffsMH <hecker.matthew@gmail.com>
2015-09-17 19:54:16 -0600

    navbar complete and login page functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,17 @@ class SessionsController < ApplicationController
   def create
     user =  User.find_by_email(params[:user][:email])
     if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
+      session[:user] = user.id
       flash[:notice] = "Welcome: #{user.user_name}"
       redirect_to controller: 'welcome', action: 'index'
     else
       flash[:notice] = "Invalid Login"
       redirect_to action: 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user)
+    redirect_to controller: "welcome", action: "index"
   end
 end
