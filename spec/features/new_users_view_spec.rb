2cfbb87bde76d156e42103011b59158b560443df
HoffsMH <hecker.matthew@gmail.com>
2015-09-17 19:07:59 -0600

    form can now successfully validate and create users also added feature test for validations

--- spec/features/new_users_view_spec.rb
+++ spec/features/new_users_view_spec.rb
@@ -6,5 +6,28 @@ describe 'the sign up page', type: :feature do
       visit '/users/new'
       expect(page).to have_css('.signup')
     end
+
+    it 'redirects to index if valid credentials' do
+      visit '/users/new'
+      fill_in("email", :with => 'somedude@gmail.com')
+      fill_in("User Name", :with => 'somedude')
+      fill_in("Password", :with => 'password')
+      fill_in("Confirm", :with => 'password')
+      click_button('Create')
+
+      expect(current_path).to eq('/')
+    end
+
+
+    it 'redirects to new if invalid credentials' do
+      visit '/users/new'
+      fill_in("email", :with => 'somedugmail.com')
+      fill_in("User Name", :with => 'somedude')
+      fill_in("Password", :with => 'pasfsfssword')
+      fill_in("Confirm", :with => 'password')
+      click_button('Create')
+
+      expect(current_path).to eq('/users/new')
+    end
   end
 end
