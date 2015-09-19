177f749303433663dd29e0f6a6a1e4a4b1d0e9d6
HoffsMH <hecker.matthew@gmail.com>
2015-09-19 16:07:00 -0600

    added role column and default to user table

---
+++ db/migrate/20150919220114_user_roles.rb
@@ -0,0 +1,5 @@
+class UserRoles < ActiveRecord::Migration
+  def change
+    add_column :users, :role, :integer, default: 1
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917230034) do
+ActiveRecord::Schema.define(version: 20150919220114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20150917230034) do
     t.string   "email"
     t.string   "user_name"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 1
   end
 
 end
